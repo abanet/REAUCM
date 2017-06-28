7b674baf998ce44af20f4563127bf9eb0ac5dc1a
ALBERTO BANET MASA <abanet@ucm.es>
2017-06-28 16:13:00 +0200

    Primera versión funcional

---
+++ REAUCMIPAD/ReaCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReaCollectionViewCell.swift
+//  REAUCMTV
+//
+//  Created by Alberto Banet Masa on 28/6/17.
+//  Copyright © 2017 UCM. All rights reserved.
+//
+
+import UIKit
+
+class ReaCollectionViewCell: UICollectionViewCell {
+    
+}
